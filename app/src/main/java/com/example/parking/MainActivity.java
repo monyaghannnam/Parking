a523663da885f5b825d2adf5833528dfc7e4d291
monyaghannam <monya.jamal.96@gmail.com>
2019-04-06 23:21:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/parking/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.parking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.database.sqlite.SQLiteDatabase;
+import java.io.*;
+import android.database.SQLException;
+public class MainActivity extends AppCompatActivity {
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDBHelper = new DatabaseHelper(this);
+
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+    }
+}
